36d6e763e5cdc074e6daadd2013a5d385df07c53
Clyde D'Souza <clyd94@gmail.com>
2023-10-07 18:39:19 +1300

    DI using ienumerable

--- ConsoleDemoApp/App.cs
+++ ConsoleDemoApp/App.cs
@@ -4,20 +4,17 @@ namespace ConsoleDemoApp;
 
 public class App
 {
-    private readonly IAnimals _domestic;
-    private readonly IAnimals _wild;
-
-    public App(IAnimals domestic, IAnimals wild)
+    public App(IEnumerable<IAnimals> animals)
     {
-        _domestic = domestic;
-        _wild = wild;
+        foreach (var animal in animals)
+        {
+            animal.PrintCharacteristics();
+        }
     }
 
     public void Run(string[] args)
     {
         Console.WriteLine("Start console app");
-        _domestic.PrintCharacteristics();
-        _wild.PrintCharacteristics();
         Console.WriteLine("End console app");
     }
 }
